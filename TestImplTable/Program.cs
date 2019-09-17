727346c39c174e5bc3b7cb9059a47c01aeb393d3
Alexandre Morel - DIVTEC <alexandre.morel@divtec.ch>
2019-09-17 16:40:40 +0200

    Création d'un projet pour les table + Dichotomie

---
+++ TestImplTable/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestImplTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TableDichotomie test = new TableDichotomie();
+            test.afficherTable();
+
+            Console.WriteLine();
+            Console.ReadKey();
+
+            int a = test.rechercherValeur(10);
+            Console.WriteLine("La valeur 10 se trouve à l'index " + a );
+            a = test.rechercherValeur(20);
+            Console.WriteLine("La valeur 20 se trouve à l'index " + a);
+             a = test.rechercherValeur(30);
+            Console.WriteLine("La valeur 30 se trouve à l'index " + a);
+             a = test.rechercherValeur(40);
+            Console.WriteLine("La valeur 40 se trouve à l'index " + a);
+             a = test.rechercherValeur(50);
+            Console.WriteLine("La valeur 50 se trouve à l'index " + a);
+             a = test.rechercherValeur(60);
+            Console.WriteLine("La valeur 60 se trouve à l'index " + a);
+             a = test.rechercherValeur(70);
+            Console.WriteLine("La valeur 70 se trouve à l'index " + a);
+            a = test.rechercherValeur(80);
+            Console.WriteLine("La valeur 80 se trouve à l'index " + a);
+            a = test.rechercherValeur(90);
+            Console.WriteLine("La valeur 90 se trouve à l'index " + a);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ TestImplTable/TableDichotomie.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestImplTable
+{
+    class TableDichotomie
+    {
+        private int[] table;
+        private int nbrValeurTable;
+
+        public TableDichotomie()
+        {
+            nbrValeurTable = 50;
+            chargerTable();
+            trierTable();
+        }
+
+        private void chargerTable()
+        {
+            table = new int[nbrValeurTable];
+
+            Random random = new Random();
+
+            for (int i = 0; i != nbrValeurTable; i++)
+            {
+                table[i] = random.Next(100);
+            }
+        }
+
+        private void trierTable()
+        {
+            Array.Sort(table);
+        }
+
+        public int rechercherValeur(int recherche)
+        {
+            return dichotomie(0, nbrValeurTable - 1, recherche);
+        }
+
+        public int dichotomie(int indexDebut, int indexFin, int recherche)
+        {
+            int pivot = indexFin / 2;
+
+            if (recherche == table[pivot])
+            {
+                return pivot;
+            } else if (recherche > table[pivot])
+            {
+                return dichotomie(pivot, indexFin, recherche);
+            }
+            else if (recherche < table[pivot])
+            {
+                return dichotomie(indexDebut, pivot, recherche);
+            }
+
+            return -1;
+        }
+
+        public void afficherTable()
+        {
+            for (int i = 0; i != nbrValeurTable; i++)
+            {
+                Console.WriteLine("index : " + i + " valeur : " + table[i]);
+            }
+        }
+    }
+}
