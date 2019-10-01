90cca6282699763cc620ca3976e6e39389b17e45
Alexandre Morel - DIVTEC <alexandre.morel@divtec.ch>
2019-10-01 16:42:03 +0200

    Ajout d'un prgramme

---
+++ Labyrinthe/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labyrinthe
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Canvas canvas;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            canvas = (Canvas)FindName("grille");
+
+            List<Position> grille = new List<Position>();
+            for (int i = 0; i == 50; i++)
+            {
+
+            }
+        }
+    }
+}
